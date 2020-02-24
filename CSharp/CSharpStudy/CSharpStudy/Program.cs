cca5a386ce82ccd48ffe53618a57492004d55f38
rumdice <rumdice@gmail.com>
2020-02-24 10:11:33 +0900

    C# 스터디 코드

---
+++ CSharp/CSharpStudy/CSharpStudy/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+
+namespace CSharpStudy
+{
+    class Coffee
+    {
+        public Coffee() { }
+    }
+
+    class Egg
+    {
+    }
+
+    class Bacon
+    {
+    }
+
+    class Toast
+    {
+    }
+
+    class Juice
+    {
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Coffee cup = PourCoffee();
+            Console.WriteLine("coffee is ready");
+
+            Egg eggs = FryEggs();
+            Console.WriteLine("eggs is ready");
+
+            Bacon bacon = FryBacon();
+            Console.WriteLine("bacon is ready");
+
+            Toast toast = ToastBread();
+            ApplyButter(toast);
+            ApplyJam(toast);
+            Console.WriteLine("toast is ready");
+
+            Juice oj = PourOrangeJuice();
+            Console.WriteLine("oj is ready");
+
+            Console.WriteLine("breakfast is ready");
+        }
+
+        private static Juice PourOrangeJuice()
+        {
+            return null;
+        }
+
+        private static void ApplyJam(Toast toast)
+        {
+            return;
+        }
+
+        private static void ApplyButter(Toast toast)
+        {
+            return;
+        }
+
+        private static Toast ToastBread()
+        {
+            return null;
+        }
+
+        private static Bacon FryBacon()
+        {
+            return null;
+        }
+
+        private static Egg FryEggs()
+        {
+            return null;
+        }
+
+        private static Coffee PourCoffee()
+        {
+            return null;
+        }
+    }
+}
+
+// 혼자서 아침식사 준비하기 - 병렬로 수행 되지 않는 비동기 작업
+// 요리에 익숙한 사람은 
+// 커피를 올려놓고 준비되길 기다리면서
+// 펜을 대우고 계란 후라이를 올리고
+// 베이컨을 튀긴다
+// 빵을 토스터기에 넣어놓고
+// 토스트에 버터와 잼을 바르고
+// 오렌지 주스 한잔 따름
+
+
+// 병렬 알고리즘 은 여러 요리사 (스레드)가 필요하다
+// 한사람은 계란만 후라이 하고
+// 또 한사람은 베이컨만 굽는다
+// 각 사람은 요리가 완성 되기전까지 독립적이다 (동기적으로 차단 됨)
+
+// https://docs.microsoft.com/ko-kr/dotnet/csharp/programming-guide/concepts/async/
